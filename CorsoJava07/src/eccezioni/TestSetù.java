6dd77760b3742b193bfb336fb887e91e2b92a216
maboglia <mauro.bogliaccino@gmail.com>
2020-06-04 13:34:43 +0200

    agg

--- CorsoJava07/src/eccezioni/TestSetù.java
+++
@@ -1,9 +0,0 @@
-package eccezioni;
-
-import java.util.ArrayList;
-
-public class TestSetù {
-	public static void main(String[] args) {
-		ArrayList<String[]> prova = new ArrayList<String[]>();
-	}	
-}
